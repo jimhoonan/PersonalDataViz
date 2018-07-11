a99153b57e7dc051b15bf5304d63983e7b65c51c
jimhoonan <timothy.coonan@gmail.com>
2018-07-11 15:28:35 -0500

    Removing dependency on celery-progress

--- fbdata/tasks.py
+++ fbdata/tasks.py
@@ -1,15 +1,7 @@
 from celery import shared_task
-from celery_progress.backend import ProgressRecorder
 from .models import Uploader, FileUpload
 import time
 
-@shared_task(bind=True)
-def test_celery_progress_bar(self, seconds):
-    progress_recorder = ProgressRecorder(self)
-    for i in range(seconds):
-        time.sleep(1)
-        progress_recorder.set_progress(i + 1, seconds)
-    return 'done'
 
 @shared_task(bind=True)
 def shared_upload_from_FileUpload(self,file_upload_id):

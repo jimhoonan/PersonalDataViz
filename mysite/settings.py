bda18bb16acff2b0e7f6f0775a011be89d316c34
jimhoonan <timothy.coonan@gmail.com>
2018-07-13 13:57:38 -0500

    updating db settings for deploy

--- mysite/settings.py
+++ mysite/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/2.0/ref/settings/
 """
 
 import os
+import dj_database_url
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -74,21 +75,17 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.0/ref/settings/#databases
 
-# DATABASES = {
+DATABASES={}
+DATABASES['default'] = dj_database_url.config(default='postgres://localhost')
+
+# DATABASES ={
 #     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#         'ENGINE': 'django.db.backends.postgresql_psycopg2',
+#         'NAME': 'personaldata',
+#         'USER': 'postgres',
+#         'PASSWORD': 'Biblical2dangerous!'
 #     }
 # }
-
-DATABASES ={
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'personaldata',
-        'USER': 'postgres',
-        'PASSWORD': 'Biblical2dangerous!'
-    }
-}
 
 
 # Password validation
